2c13c01249f65f92ad85bba172d6efea7fde21cb
JOJO <JoJo2021up@protonmail.com>
2021-12-29 19:05:09 +0800

    update

--- dist/config/bondings.js
+++ dist/config/bondings.js
@@ -5,7 +5,7 @@ const web3_js_1 = require("@solana/web3.js");
 exports.mainnetBondings = [];
 exports.devnetBondings = [
     {
-        address: new web3_js_1.PublicKey('4ZUoQziFMLyfstmebiQHvaUyU1Dw7KWfd8JfKXmb7AnU'),
+        address: new web3_js_1.PublicKey('8bT37j6LSRrmkQShndDFG3E9TURkJfWVJafHrRPgmMgZ'),
         staking: new web3_js_1.PublicKey('3sHcGhf9YN9DTRvHM33s7T4ZvqtSTAn1mLASHyg4mufs'),
         payoutAsset: new web3_js_1.PublicKey('PNGmGQ7SwKTHHPCRgnznYbyTPkrAxcAPLqpgNDYNP1g')
     }
--- src/config/bondings.ts
+++ src/config/bondings.ts
@@ -6,7 +6,7 @@ export const mainnetBondings: BondingConfig[] = [
 
 export const devnetBondings: BondingConfig[] = [
   {
-    address: new PublicKey('4ZUoQziFMLyfstmebiQHvaUyU1Dw7KWfd8JfKXmb7AnU'),
+    address: new PublicKey('8bT37j6LSRrmkQShndDFG3E9TURkJfWVJafHrRPgmMgZ'),
     staking: new PublicKey('3sHcGhf9YN9DTRvHM33s7T4ZvqtSTAn1mLASHyg4mufs'),
     payoutAsset: new PublicKey('PNGmGQ7SwKTHHPCRgnznYbyTPkrAxcAPLqpgNDYNP1g')
   }
